6e6336432f963e69775199dd35f8f6de150b3c01
Dmitry F <dmitrii_fuchko@mail.ru>
2023-03-30 00:01:12 +0300

    add celery, redis, flower in docker-compose. TODO: loggers, followers endpoint, chat, email(celery) distribution, cache(redis)

---
+++ social_network/core/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+from celery import Celery
+
+
+load_dotenv()
+BROKER_HOST = os.environ.get('REDIS_HOST', default='127.0.0.1')
+BROKER_PORT = os.environ.get('REDIS_PORT', default='6379')
+
+celery = Celery(
+    'tasks',
+    broker=f'redis://{BROKER_HOST}:{BROKER_PORT}/0',
+    backend=f'redis://{BROKER_HOST}:{BROKER_PORT}/1'
+)
+
+celery.autodiscover_tasks()
